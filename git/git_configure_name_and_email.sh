9ff12b7a0d9044328c78141d89ca5d89a91c0bdc
Leonardo Taschetto <leonardotaschetto@gmail.com>
2026-06-09 21:59:07 -0300

    git config

---
+++ git/git_configure_name_and_email.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+git config --global user.name "Leonardo Taschetto"
+git config --global user.email "leonardotaschetto@gmail.com"
+sudo git config --global user.email "thedandare@gmail.com"
+sudo  git config --global user.name "Leo T."
+git config --global --add safe.directory /ram/code/fibo
+sudo git config --global --add safe.directory /ram/code/fibo
+git config --global --add safe.directory /ram/code/genui_server
+sudo git config --global --add safe.directory /ram/code/genui_server
