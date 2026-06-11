c2f62a4e0adb6e13c2e5ab788089d0e542fc4cc5
leo <thedandare@gmail.com>
2026-06-10 23:28:05 -0300

    auto-commit $(uname -a) em $(date)

--- util/gitpush.sh
+++ util/gitpush.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 echo $1
+export GIT_SSH_COMMAND="ssh -i ~/.ssh/tdd_id_ed25519"
 if [ -n $1 ]; then
     git add $1 -A
+    git commit -m 'auto-commit $(uname -a) em $(date) '
+
 else
     git status | grep Untracked
+    read -n 1 -p 'Commit?' answer
+    case "${answer,,}" in
+    s/y)
+        git commit -m 'auto-commit $(uname -a) em $(date) '
+        ;;
+    esac
 fi
-git commit -m 'auto-commit $(uname -a) em $(date) '
+
 git push git@github.com:thedandare/ubunix.git
