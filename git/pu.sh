8956a8c20ed03c35594c01a91fb876a837e8ad90
leo <thedandare@gmail.com>
2026-06-11 05:05:05 -0300

    git scriptig

--- git/pu.sh
+++ git/pu.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 echo $1
 export GIT_SSH_COMMAND="ssh -i ~/.ssh/tdd_id_ed25519"
+export Fg=\\033[0;
+export Fb=\033[1;
+export R=31m
+export G=32m
+export B=34m
+echo -e "$Fb$G tes $Fb$B te "
+
 if [ -n $1 ]; then
     echo -e  "\[0;31m parama 1:$1"
     git add $1 -A
